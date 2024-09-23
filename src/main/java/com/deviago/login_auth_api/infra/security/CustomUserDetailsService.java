3232a3f2c1c3726057dd0213c8064f94adeb0598
iago-monteirog <iagomgarcia9000@gmail.com>
2024-09-23 19:22:31 -0300

    add custom user detais service and security config

---
+++ src/main/java/com/deviago/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.deviago.login_auth_api.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.deviago.login_auth_api.domain.user.User;
+import com.deviago.login_auth_api.repositories.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+  @Autowired
+  private UserRepository uRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = this.uRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+        new ArrayList<>());
+  }
+}
